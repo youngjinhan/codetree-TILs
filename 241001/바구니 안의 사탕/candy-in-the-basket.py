9d3f969456eed8ce923f53ef2778317443a434fa
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-01 21:08:52 +0900

    Title: 바구니 안의 사탕, Time: 160ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 241001/바구니 안의 사탕/candy-in-the-basket.py
+++ 241001/바구니 안의 사탕/candy-in-the-basket.py
@@ -1,49 +1,49 @@
-# pos = [0] * 1000001
-# n, k = map(int, input().split())
-# max_p = 0
-# for i in range(n):
-#     cnt, p = map(int, input().split())
-#     max_p = max(max_p, p)
-#     pos[p] += cnt
+pos = [0] * 1000001
+n, k = map(int, input().split())
+max_p = 0
+for i in range(n):
+    cnt, p = map(int, input().split())
+    max_p = max(max_p, p)
+    pos[p] += cnt
 
-# p_sum = 0
-# for i in range(1, 2*k + 2):
-#     p_sum += pos[i]
+p_sum = 0
+for i in range(1, 2*k + 2):
+    p_sum += pos[i]
 
-# ans = p_sum
+ans = p_sum
 
-# for i in range(2, p - (2*k) + 1):
-#     p_sum = p_sum - pos[i - 1] + pos[i + 2*k]
-#     ans = max(ans, p_sum)
+for i in range(2, p - (2*k) + 1):
+    p_sum = p_sum - pos[i - 1] + pos[i + 2*k]
+    ans = max(ans, p_sum)
 
-# print(ans)
+print(ans)
 
 
-N, K = map(int, input().split())
+# N, K = map(int, input().split())
  
-maxSize = 1000001
-arr = [0] * maxSize
-basket = set()
-for i in range(N):
-    a, b = map(int, input().split())
-    arr[b] += a
-    basket.add(b)
+# maxSize = 1000001
+# arr = [0] * maxSize
+# basket = set()
+# for i in range(N):
+#     a, b = map(int, input().split())
+#     arr[b] += a
+#     basket.add(b)
  
-j = 0
-k = 0
-jSum = 0
-kSum = 0
-res = 0
+# j = 0
+# k = 0
+# jSum = 0
+# kSum = 0
+# res = 0
  
-for i in range(maxSize):
-    while j < i - K:
-        jSum += arr[j]
-        j += 1
+# for i in range(maxSize):
+#     while j < i - K:
+#         jSum += arr[j]
+#         j += 1
     
-    while k <= i + K and k < maxSize:
-        kSum += arr[k]
-        k += 1
+#     while k <= i + K and k < maxSize:
+#         kSum += arr[k]
+#         k += 1
     
-    res = max(res, kSum - jSum)
+#     res = max(res, kSum - jSum)
     
-print(res)
+# print(res)
