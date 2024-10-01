04debf0b6b31abc40234fdc479fdff5a4a00d2a3
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-01 20:43:13 +0900

    Title: 바구니 안의 사탕, Time: 160ms, Memory: 32MB, Status: Wrong Answer - CodeTree

---
+++ 241001/바구니 안의 사탕/candy-in-the-basket.py
@@ -0,0 +1,19 @@
+pos = [0] * 1000001
+n, k = map(int, input().split())
+max_p = 0
+for i in range(n):
+    cnt, p = map(int, input().split())
+    max_p = max(max_p, p)
+    pos[p] += cnt
+
+p_sum = 0
+for i in range(1, 2*k + 2):
+    p_sum += pos[i]
+
+ans = p_sum
+
+for i in range(2, p - (2*k+1) + 2):
+    p_sum = p_sum - pos[i - 1] + pos[i + 2*k]
+    ans = max(ans, p_sum)
+
+print(ans)
