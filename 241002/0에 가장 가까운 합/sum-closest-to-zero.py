435897dd40867b73d1778d855d998cc3fabca913
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-02 23:54:42 +0900

    Title: 0에 가장 가까운 합, Time: 132ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 241002/0에 가장 가까운 합/sum-closest-to-zero.py
+++ 241002/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -6,7 +6,7 @@ ans = 10**10
 for i in range (1, n+1):
     p_sum = arr[i]
     while j > i and abs(p_sum + arr[j]) <= ans:
-        ans = min(ans, abs(p_sum + arr[j]))
+        ans = abs(p_sum + arr[j])
         j -= 1
 
     if ans == 0:
