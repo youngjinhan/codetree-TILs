daf89abcf69d92f43f0e2d5628d9bd265cf4b788
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-03 16:13:01 +0900

    Title: 0에 가장 가까운 합, Time: 136ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 241003/0에 가장 가까운 합/sum-closest-to-zero.py
+++ 241003/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -1,26 +1,17 @@
-import sys
-
-INT_MAX = sys.maxsize
-
-# 변수 선언 및 입력:
 n = int(input())
-arr = list(map(int, input().split()))
+arr = [0] + sorted(list(map(int, input().split())))
 
-arr = [0] + sorted(arr)
-
-# 0에 가장 가까운 합을 구합니다.
-ans = INT_MAX
-
-# 구간을 잡아봅니다.
 j = n
-for i in range(1, n + 1):
-    if i < j:
+ans = 10**10
+for i in range (1, n+1):
+    if j > i:
         ans = min(ans, abs(arr[i] + arr[j]))
     
-    # 두 수의 합이 0 이하가 될 때 까지 j 구간을 내리면서 정답을 살펴봅니다.
-    while i < j - 1 and arr[i] + arr[j] > 0:
+    while i < j-1 and arr[i] + arr[j-1] > 0:
+        if abs(arr[i] + arr[j-1]) == 0:
+            ans = 0
+            exit
+        ans = min(ans, abs(arr[i] + arr[j-1]))
         j -= 1
-        ans = min(ans, abs(arr[i] + arr[j]))
 
-# 정답을 출력합니다.
 print(ans)
